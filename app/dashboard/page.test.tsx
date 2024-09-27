aeee364aac682b308943877b9da49606cf13972c
jha <haimezjohn@gmail.com>
2024-09-27 20:53:17 +0200

    feat : add vitest for testing and add test for first page

---
+++ app/dashboard/page.test.tsx
@@ -0,0 +1,9 @@
+import { test, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Home from './page'
+
+test('Page', () => {
+  render(<Home />)
+  const title = screen.getByRole('heading', { level: 1, name: 'Dashboard' })
+  expect(title).toBeDefined()
+})
