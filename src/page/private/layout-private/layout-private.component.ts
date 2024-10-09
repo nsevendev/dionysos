da00dcbe3be140e846a2ec3a93e5e3c292152367
jha <haimezjohn@gmail.com>
2024-10-09 20:43:11 +0200

    init started project

---
+++ src/page/private/layout-private/layout-private.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-layout-private',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './layout-private.component.html',
+  styleUrl: './layout-private.component.css'
+})
+export class LayoutPrivateComponent {
+
+}
--- src/root/root.routes.ts
+++ src/root/root.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import {LayoutPrivateComponent} from '../page/private/layout-private/layout-private.component';
+import {LayoutPublicComponent} from '../page/public/layout-public/layout-public.component';
+import {HomeComponent} from '../page/public/home/home.component';
+import {DashboardComponent} from '../page/private/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', // path '/'
+    component: LayoutPublicComponent,
+    children: [
+      {
+        path: '', // path '/'
+        component: HomeComponent
+      }
+    ]
+  },
+  {
+    path: 'dashboard', // path '/dashboard'
+    component: LayoutPrivateComponent,
+    children: [
+      {
+        path: '', // path '/dashboard'
+        component: DashboardComponent
+      }
+    ]
+  },
+];
