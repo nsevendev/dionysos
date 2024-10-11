99980e44dbb9babe699cd3d488fcd0b996547c5e
YannisGhartouchent <ghartouchent@yahoo.com>
2024-10-11 14:41:24 +0200

    navigation#4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{html,ts,css,scss}",
-    "./src/**/*.spec.ts"
-  ],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
