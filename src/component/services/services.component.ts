ab001c86537fe1e7fbf8d02608da0cfc9703af55
LutfuS <us.lutfu25@gmail.com>
2024-10-23 14:25:38 +0200

    feat/#7-NosService-ajoutDossierCore

--- src/component/services/services.component.ts
+++ src/component/services/services.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CardComponent } from './card/card.component';
+import { CardComponent } from '../core/card/card.component';
 
 @Component({
   selector: 'app-services',
